d758cafc017ae84a3e4e7055f46349252c3d23f7
David Colgan <dvcolgan@gmail.com>
2014-08-25 20:21:30 -0400

    build

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -16,6 +16,19 @@ public static class Constants
 		"The antagonist was THE MAN HIMSELF",
 		"I feel as if I am the villain!",
 		"I think you used the wrong theme, said Gaia",
-		"Oops, said the man."
+		"Oops, said the man.",
+		"Welcome to This Game!",
+		"Once upon a time there was a man.",
+		"A man who wanted to do a thing.",
+		"He pushed crates far and wide.",
+		"I think something is wrong, he said.",
+		"Thus we have an antagonist.",
+		"The antagonist was THE MAN HIMSELF",
+		"I feel as if I am the villain!",
+		"I think you used the wrong theme, said Gaia",
+        "Oops, said the man.",
+		"Oops, said the man.",
+		"Oops, said the man.",
+		"YOU WIN, ALSO CONNECTED WORLDS"
 	};
 }
